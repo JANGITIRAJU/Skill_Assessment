0aeff5d00c793d25fb9953839f990b84177ebc94
JangitiRaju <jrajuyadav9@gmail.com>
2024-01-05 17:03:04 +0530

    Changes in Assessment1

--- Java/Sum.java
+++ Java/Sum.java
@@ -3,7 +3,7 @@ package Java;
 public class Sum {
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 100;
         int sum = a + b;
         System.out.println(sum);
        
