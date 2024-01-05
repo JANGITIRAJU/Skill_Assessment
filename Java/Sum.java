e846c18a355a8a3b59afb75557010bf8c32af494
JangitiRaju <jrajuyadav9@gmail.com>
2024-01-05 15:57:42 +0530

    New

--- Java/Sum.java
+++ Java/Sum.java
@@ -2,8 +2,11 @@ package Java;
 
 public class Sum {
     public static void main(String[] args) {
-        System.out.println("S");
-        System.out.println("R");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+       
     }
     
 }
